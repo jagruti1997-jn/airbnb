ae2f5eb7ede6a60af6ba13dd835b72cff7a8a4f3
jagruti nand <jagrutinand77@gmail.com>
2024-02-01 14:35:16 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -7,11 +7,7 @@ import {BrowserRouter,Route,Routes} from 'react-router-dom'
 function App() {
   return (
    <>
-   <BrowserRouter>
-   <Routes>
-    <Route path="/" element={<Home/>}></Route>
-   </Routes>
-   </BrowserRouter>
+  <Home/>
    
    </>
   
